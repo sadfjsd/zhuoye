e14995904c44a9758522022ab246b75650f95465
sadfjsd <3061164780@qq.com>
2021-06-05 14:42:37 +0800

    作业

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,403 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="content">
+  <div class="b1">快帮网
+    <img src="./icon/省略号@.png" class="dian" />
   </div>
+
+  <div class="b2">合肥
+            <img src="./icon/向下@.png" class="xia" />
+            <img src="./icon/搜索@3.png" class="shousuo" />
+             <input type="text" class="kuang" placeholder="找工作找房子" />
+  </div>
+  <div>
+    <img src="./icon/找房@3.png" class="zhaofang" />
+    <img src="./icon/房产@3.png" class="fangchan" />
+      <img src="./icon/招聘@3.png" class="zhaopin" />
+      <img src="./icon/求职@3.png" class="qiuzhi" />
+  </div>
+<div>
+  <class="zf"/>免费找店
+    <class="fc"/>房产
+      <class="zp"/>招聘
+        <class="qz"/>求职
+          </div>
+<div>
+  <img src="./icon/万能帮@3.png" class="bang" />
+  <img src="./icon/二手交易@3.png" class="er" />
+  <img src="./icon/快购@3.png" class="kuaigou" />
+  <img src="./icon/爱心公益@3.png" class="aixin" />
+</div>
+<div>
+  <class="wn"/>万能帮
+    <class="es"/>二手交易
+      <class="kg"/>快购
+        <class="ax"/>爱心公益
+          </div>
+<div class="b3">
+  <img src="./icon/点@3.png" class="d1" />
+  <img src="./icon/点@3.png" class="d2" />
+  <img src="./icon/点@3.png" class="d3" />
+</div>
+<div>
+  <class="sy"/>生意转让
+    <class="cz"/>房屋出租
+      <class="qz"/>房屋求租
+        </div>
+        <div>
+          <class="space">
+            <class="space1">
+              </div>
+<div>
+  <img src="./icon/图片@3.png" class="tupian" />
+  <class="wenzi"/>业主直转！淮河路步行街旺铺餐馆
+  <class="wenzi1"/>庐阳 - 淮河路步行街 | 经营中
+  <class="wenzi2"/>刘先生 张震牛面馆
+</div>
+<div>
+<img src="./icon/图片2@3.png" class="tupian2" />
+<class="wenzi3"/>业主直转！淮河路步行街旺铺餐馆
+  <class="wenzi4"/>庐阳 - 淮河路步行街 | 经营中
+    <class="wenzi5"/>刘先生 张震牛面馆
+      </div>
+      <div>
+        <class="space2">
+          </div>
+<div>
+  <img src="./icon/首页@3.png" class="shouye" />
+  <img src="./icon/查找@3.png" class="chazhao" />
+  <img src="./icon/个人中心@3.png" class="geren" />
+</div>
+<div>
+  <class="wz"/>首页
+<class="wz1"/>查找
+<class="wz2"/>个人中心
+</div>
+</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+export default {}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+
+<style lang="scss" scoped>
+.content{
+  .b1{
+    width: 375px;
+    height: 44px;
+    background: #FFFFFF;
+    border-radius: 12px 12px 0px 0px;
+  }
+  .dian{
+    width: 16px;
+height: 2px;
+background: rgba(0, 0, 0, 0.9);
+  }
+  .b2{
+    width: 24px;
+    height: 17px;
+    font-size: 12px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #333333;
+    line-height: 17px;
+  }
+  .xia{
+    width: 11px;
+height: 6px;
+  }
+  .shousuo{
+    width: 16px;
+height: 16px;
+background: #CCCCCC;
+  }
+  .kuang{
+    width: 242px;
+height: 30px;
+background: #F9F9F9;
+border-radius: 15px;
+  }
+.zhaofang{
+  width: 32px;
+height: 32px;
+background: #000000;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.fangchan{
+  width: 25px;
+height: 25px;
+background: #0E8ED8;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.zhaopin{
+  width: 23px;
+height: 26px;
+background: #F5A623;
 }
-a {
-  color: #42b983;
+.qiuzhi{
+  width: 26px;
+height: 23px;
+background: #7ED321;
+}
+.zf{
+width: 48px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.fc{
+  width: 24px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.zp{
+  width: 24px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.qz{
+  width: 24px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.bang{
+  width: 24px;
+height: 26px;
+background:#F5A623
+}
+.er{
+  width: 26px;
+height: 25px;
+background: #7ED321;
+}
+.kuaigou{
+  width: 26px;
+height: 23px;
+background: #0E8ED8;
+}
+.aixin{
+  width: 26px;
+height: 23px;
+background: #FF552E;
+}
+.wn{
+  width: 36px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.es{
+  width: 48px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.kg{
+  width: 24px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.ax{
+  width: 48px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.b3{
+  width: 300px;
+height: 44px;
+background: #FF552E;
+border-radius: 22px;
+}
+.d1{
+  width: 8px;
+height: 8px;
+background: #FFFFFF;
+opacity: 0.7;
+}
+.d2{
+  width: 8px;
+height: 8px;
+background: #FFFFFF;
+opacity: 0.7;
+}
+.d3{
+  width: 8px;
+height: 8px;
+background: #FFFFFF;
+opacity: 0.7;
+}
+.sy{
+  width: 48px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Medium, PingFang SC;
+font-weight: 500;
+color: #FF552E;
+line-height: 17px;
+}
+.cz{
+  width: 48px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #0E8ED8;
+line-height: 17px;
+}
+.qz{
+  width: 48px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #0E8ED8;
+line-height: 17px;
+}
+.space{
+  width: 375px;
+height: 1px;
+}
+.space1{
+  width: 66px;
+height: 1px;
+background:#FF552EFF
+}
+.tupian{
+  width: 150px;
+height: 80px;
+background: #D8D8D8;
+}
+.wenzi{
+  width: 175px;
+height: 34px;
+font-size: 12px;
+font-family: PingFangSC-Medium, PingFang SC;
+font-weight: 500;
+color: #333333;
+line-height: 17px;
+}
+.wenzi1{
+  width: 158px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.wenzi2{
+  width: 100px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.tupian2{
+  width: 150px;
+height: 80px;
+}
+.wenzi3{
+  width: 175px;
+height: 34px;
+font-size: 12px;
+font-family: PingFangSC-Medium, PingFang SC;
+font-weight: 500;
+color: #333333;
+line-height: 17px;
+}
+.wenzi4{
+  width: 158px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.wenzi5{
+  width: 100px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #666666;
+line-height: 17px;
+}
+.space2{
+  width: 375px;
+height: 50px;
+background: #FFFFFF;
+}
+.shouye{
+  width: 19px;
+height: 18px;
+background: #FF552E;
+}
+.chazhao{
+  width: 18px;
+height: 18px;
+background: #666666;
+}
+.geren{
+  width: 24px;
+height: 24px;
+background: #000000;
+}
+.wz{
+  width: 100px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Medium, PingFang SC;
+font-weight: 500;
+color: #FF552E;
+line-height: 17px;
+}
+.wz1{
+  width: 100px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Medium, PingFang SC;
+font-weight: 500;
+color: #666666;
+line-height: 17px;
+}
+.wz2{
+  width: 100px;
+height: 17px;
+font-size: 12px;
+font-family: PingFangSC-Medium, PingFang SC;
+font-weight: 500;
+color: #666666;
+line-height: 17px;
+}
 }
 </style>
